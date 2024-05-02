75f652fc1949cab0b9845a44c95f9860c28e73ea
olga_demina <olga.demina1410@gmail.com>
2024-05-02 13:08:00 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+  <h1>Hello world</h1>
+
+  <div class="banner">
+    <h2>Banner title</h2>
+  </div>
+
+  <h2>Section title</h2>
+
+</template>
+
+<style lang="scss">
+
+
+</style>
